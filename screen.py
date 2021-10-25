29e0db4d06478afdb2d39e064a13a1b6f07b14c6
Evgeny Grebnev <egrebnev@infinidat.com>
2021-10-25 08:43:09 +0300

    prevent color change on the same pressed key

--- screen.py
+++ screen.py
@@ -10,9 +10,15 @@ screen = pygame.display.set_mode((displ_info.current_w, displ_info.current_h))
 colors = tuple(color for color in [random.randint(0, 255) for _ in range(0, 3)])
 screen.fill(colors)
 pygame.display.flip()
+prev_key = None
 
 
 def update_colors(key):
+    global prev_key
+    if prev_key == key:
+        pygame.display.flip()
+        return
+    prev_key = key
     colors2 = tuple(color for color in [random.randint(0, 255) for _ in range(0, 3)])
     screen.fill(colors2)
     pygame.display.flip()
