1ba75c940ce88eb0d2254675c70020f970a281eb
Evgeny Grebnev <egrebnev@infinidat.com>
2021-10-25 09:52:03 +0300

    to OOP style

--- screen.py
+++ screen.py
@@ -2,38 +2,46 @@ from pynput import keyboard
 import pygame
 import random
 
-pygame.init()
-displ_info = pygame.display.Info()
 
-screen = pygame.display.set_mode((displ_info.current_w, displ_info.current_h))
+class LeonardsGame:
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_caption("Leonard's Super Hard Game")
+        displ_info = pygame.display.Info()
+        self.screen = pygame.display.set_mode((displ_info.current_w, displ_info.current_h))
+        self.change_colors()
+        self.prev_key = None
+        self.lst = keyboard.Listener(on_press=self._update_colors)
+        self.lst.start()
 
-colors = tuple(color for color in [random.randint(0, 255) for _ in range(0, 3)])
-screen.fill(colors)
-pygame.display.flip()
-prev_key = None
+    def _update_colors(self, key):
+        if self.prev_key == key and key == keyboard.Key.esc:
+            self.lst.stop()
+            pygame.quit()
+        if self.prev_key == key:
+            pygame.display.flip()
+            return
+        self.prev_key = key
+        self.change_colors()
 
+    @staticmethod
+    def _get_random_colors() -> tuple:
+        return tuple(color for color in [random.randint(0, 255) for _ in range(0, 3)])
 
-def update_colors(key):
-    global prev_key
-    if prev_key == key and key == keyboard.Key.esc:
-        lst.stop()
-        pygame.quit()
-    if prev_key == key:
+    def start(self):
+        while True:
+            event = pygame.event.poll()
+
+            if event.type == pygame.QUIT:
+                self.lst.stop()
+                pygame.quit()
+
+    def change_colors(self):
+        colors = self._get_random_colors()
+        self.screen.fill(colors)
         pygame.display.flip()
-        return
-    prev_key = key
-    colors2 = tuple(color for color in [random.randint(0, 255) for _ in range(0, 3)])
-    screen.fill(colors2)
-    pygame.display.flip()
 
 
-lst = keyboard.Listener(on_press=update_colors)
-lst.start()
-
-
-while True:
-    event = pygame.event.poll()
-
-    if event.type == pygame.QUIT:
-        lst.stop()
-        pygame.quit()
+if __name__ == "__main__":
+    lenny_game = LeonardsGame()
+    lenny_game.start()
